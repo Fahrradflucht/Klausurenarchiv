64245a07cb65b7c1591d80966e135e95ba0a80a0
Fahrradflucht <mail@mathiswiehl.de>
2016-09-22 14:32:56 +0200

    Add semesters to uploads
    Closes #6

--- web/views/upload_view.ex
+++ web/views/upload_view.ex
@@ -1,3 +1,17 @@
 defmodule Klausurenarchiv.UploadView do
   use Klausurenarchiv.Web, :view
+
+  def year_list do
+    2005..DateTime.utc_now.year
+    |> Enum.to_list()
+    |> Enum.flat_map(fn(y) ->
+      [to_short_string(y), "#{to_short_string(y)}/#{to_short_string(y+1)}"]
+    end)
+  end
+
+  defp to_short_string(year) do
+    year
+    |> to_string()
+    |> String.slice(-2..-1)
+  end
 end
