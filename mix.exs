262f4150a2ffa3b2ea3b3cf95f42492cffeba9c6
Fahrradflucht <mail@mathiswiehl.de>
2016-09-26 12:50:42 +0200

    Release 1.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Klausurenarchiv.Mixfile do
 
   def project do
     [app: :klausurenarchiv,
-     version: "1.0.3",
+     version: "1.0.4",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
