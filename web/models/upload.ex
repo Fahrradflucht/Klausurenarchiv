0039e11c4d4f08868df7ced5824aab03f41f8cc7
Fahrradflucht <mail@mathiswiehl.de>
2016-10-06 10:34:57 +0200

    Add support for resit exams

--- web/models/upload.ex
+++ web/models/upload.ex
@@ -5,6 +5,7 @@ defmodule Klausurenarchiv.Upload do
     field :description, :string
     field :files, {:array, :string}
     field :semester, :string
+    field :is_resit, :boolean
     belongs_to :instructor, Klausurenarchiv.Instructor
     belongs_to :user, Klausurenarchiv.User
 
@@ -16,7 +17,7 @@ defmodule Klausurenarchiv.Upload do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:description, :files, :semester])
+    |> cast(params, [:description, :files, :semester, :is_resit])
     |> validate_required([:semester, :files])
   end
 end
