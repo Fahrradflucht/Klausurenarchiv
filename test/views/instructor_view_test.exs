71919b99b216cf5f492480678a60a8a2dfe7fec7
Fahrradflucht <mail@mathiswiehl.de>
2016-10-07 19:28:29 +0200

    Fix grammar error

---
+++ test/views/instructor_view_test.exs
@@ -0,0 +1,18 @@
+defmodule Klausurenarchiv.InstructorViewTest do
+  use Klausurenarchiv.ConnCase, async: true
+  
+  alias Klausurenarchiv.InstructorView
+  import Phoenix.View
+
+  alias Klausurenarchiv.Course
+  
+  test "index.html contains new instructor link" do
+    course = Repo.insert! %Course{name: "Herbology"}
+    assert render_to_string(
+        InstructorView,
+        "index.html",
+        [course: course, instructors: [], conn: build_conn()]
+    ) =~ "Neue*r Lehrende*r"
+  end
+  
+end
